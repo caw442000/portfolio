e34ca362e3e1e4b1d1a315ad366c7986bb593b22
cedric-winbush <cawinbushjr@gmail.com>
2020-03-30 11:21:48 -0500

    add scaffolding of header, projects, footer

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  );
+};
+export default Header;
---
+++ src/components/projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Projects = props => {
+  return (
+    <div>
+      <p>Projects</p>
+    </div>
+  );
+};
+export default Projects;
